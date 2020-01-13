6bfadade9336c58661a25c6314504c4d8ae93d55
Nikola Okuka <nikolaokuka@gmail.com>
2020-01-13 18:37:13 +0100

    finish SignIn handleSubmit method

--- src/App.js
+++ src/App.js
@@ -31,8 +31,6 @@ class App extends Component {
               ...snapShot.data()
             }
           })
-
-          console.log(this.state.currentUser)
         })
       } else {
         this.setState({ currentUser: userAuth })
