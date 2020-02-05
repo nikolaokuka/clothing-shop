94fec0b445c4a5048ed6e4cd48a8c7af2748dd9b
Nikola Okuka <nikolaokuka@gmail.com>
2020-02-05 12:13:38 +0100

    add CheckoutPage component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import { createStructuredSelector } from 'reselect'
 
 import HomePage from './pages/HomePage/HomePage'
 import ShopPage from './pages/ShopPage/ShopPage'
-import Header from './components/Header/Header'
 import SignPage from './pages/SignPage/SignPage'
+import CheckoutPage from './pages/CheckoutPage/CheckoutPage'
+
+import Header from './components/Header/Header'
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
@@ -47,6 +49,7 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path='/sign' render={() => this.props.currentUser ? <Redirect to='/' /> : <SignPage />} />
         </Switch>
       </div >
